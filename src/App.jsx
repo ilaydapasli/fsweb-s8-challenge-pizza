9bc855f1417cc0dfdee393a1232c9ba18771388d
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-03 17:35:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { useHistory } from 'react-router-dom';
 
 import './App.css'
 import Home from './components/Home'
