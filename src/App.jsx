dde06ef33705e6cbe9ce77523fb7dc7e1dacb060
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-03 16:14:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,34 @@ import './App.css'
 import Home from './components/Home'
 import Success from './components/Success'
 import OrderPizza from "./components/OrderPizza"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
- 
+ const [orderedData,setorderedData]=useState(null);
+
+ const handleOrder=(s)=>{
+  setorderedData(s)
+ }
 
   return (
-    <>
+    <Router>
+      <Switch>
+      <Route exact path="/" >
+
+        <Home />
+      </Route>
+      <Route path="/siparis-olustur">
+    <OrderPizza onSubmit={handleOrder}/>
+      </Route>
+      <Route path="/siparis-alindi">
+        <Success orderedData={orderedData}  />
+      </Route >
+      </Switch>
+
+
+
+    </Router>
    
-   <OrderPizza />
-      
-    </>
   )
 }
 
