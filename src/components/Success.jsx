8e586cccb2b7f64d82fe3a652e708a2c9344d988
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-03 16:14:49 +0300

    Update Success.jsx

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,9 +2,16 @@
 import Logos  from "../assets/logo.svg";
 import "./Success.css"
 import React,{useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
+
 
 
 export default function Success(){
+
+    const history =useHistory();
+   
+
+
     useEffect(() => {
         document.body.className = "success-body";
       }, []);
