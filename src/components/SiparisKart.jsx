392d8de5290ccac85b71d744dade744bdfefe7fc
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-05 03:04:05 +0300

    Update SiparisKart.jsx

--- src/components/SiparisKart.jsx
+++ src/components/SiparisKart.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Button, Card, CardImg, CardImgOverlay, CardSubtitle, CardText, CardTitle, } from 'reactstrap'
+import { Button, Card, CardImg, CardImgOverlay, CardSubtitle, CardText, CardTitle, Navbar, NavbarBrand, } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import pizzaBack from "../../Assets/Iteration-2-aseets/cta/kart-1.png";
 import burgerBack from "../../Assets/Iteration-2-aseets/cta/kart-2.png";
 import motoBack from "../../Assets/Iteration-2-aseets/cta/kart-3.png";
+import koreLogo from "../../Assets/Iteration-2-aseets/icons/1.svg";
+import pizza from "../../Assets/Iteration-2-aseets/icons/2.svg";
+import burger from "../../Assets/Iteration-2-aseets/icons/3.svg";
+import kızartma from "../../Assets/Iteration-2-aseets/icons/4.svg";
+import fastfood from "../../Assets/Iteration-2-aseets/icons/5.svg";
+import gazliIcecek from "../../Assets/Iteration-2-aseets/icons/6.svg";
 import "./SiparisKart.css"
 import Gorseller from './gorseller';
+
 function SiparisKart() {
-    document.body.style.backgroundColor = "beige";
+    document.body.style.backgroundColor = "rgb(245, 245, 235)";
 
   return (
 <>
@@ -99,7 +106,24 @@ function SiparisKart() {
     </div>
     </div>
     </div>
+    <div className='siparis-text'>
+    <span className="text-danger"> en çopk paketlenen menüler </span><br />
+    <h1> Acıktıran Kodlara Doyuran Lezzetler</h1>
+    </div>
+    <div className='second-menu-container'>
+    <Navbar>
+        <NavbarBrand href="">
+            <img src={koreLogo} alt="" />
+            Ramen</NavbarBrand>
+        <NavbarBrand href=""><img src={pizza} alt="" />Pizza</NavbarBrand>
+        <NavbarBrand href=""> <img src={burger} alt="" /> Burger</NavbarBrand>
+        <NavbarBrand href=""><img src={kızartma} alt="" />French fries</NavbarBrand>
+        <NavbarBrand href=""><img src={fastfood} alt="" />Fast Food</NavbarBrand>
+        <NavbarBrand href=""><img src={gazliIcecek} alt="" />Soft drinks</NavbarBrand>
 
+        
+      </Navbar>
+      </div>
     <Gorseller />
    </>
   )
