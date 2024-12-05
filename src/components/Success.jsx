429a41d2edf3a96f68b504ba9aaba166a16771ea
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-05 17:39:34 +0300

    Update Success.jsx

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,27 +3,30 @@ import Logos  from "../assets/logo.svg";
 import "./Success.css"
 import React,{useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Success({orderedData}){
 
     const history =useHistory();
    
+    document.body.style.backgroundColor = "red";
 
-
-    useEffect(() => {
-        document.body.className = "success-body";
-      }, []);
+   
   
     return(
 
         <>
-        <header className="success-page">
-            <img src={Logos} alt="Logo" />
-            <h1 className='main-title'>TEBRİKLER! <br />
-            SİPARİŞ ALINDI</h1>
-            </header>
+        <div className="bg-main">
+        <div className="success-page">
+        <header >  <img src={Logos} alt="Logo" /> </header>
+        <div className="head-texts"></div>
+        <span className="lezzet-span" style={{color:"yellow"}}> lezzetin yolda </span><br />
+        <h1> SİPARİŞ ALINDI</h1>
+        
+        <hr class="custom-line" />        
+        </div>
+        </div>
             </>
     )
 }
